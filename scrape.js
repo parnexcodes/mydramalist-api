b4085c8ee5fa68bd90e3adcef9d19b521e782a63
parnexcodes <parnexcodes@gmail.com>
2022-06-16 18:16:15 +0530

    fix fetch -> node-fetch

--- scrape.js
+++ scrape.js
@@ -1,6 +1,6 @@
 // Declare a route
 const cheerio = require('cheerio')
-import fetch from "node-fetch";
+const fetch = require('node-fetch')
 
 const scrape = async (fastify, options) => {
     fastify.get('/:type/:filter', async (request, reply) => {
